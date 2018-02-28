4f8a120165a5c2a898e634d2882ada7c2cd045eb
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2018-02-28 15:34:02 +0100

    Implemented the creation of the CYK chart and a method to print it

--- cyk_parser.py
+++ cyk_parser.py
@@ -1,4 +1,6 @@
-from ast import AST
+from collections import defaultdict
+
+from cfg import CFG
 
 
 class CYKParser:
@@ -14,31 +16,101 @@ class CYKParser:
 
     @staticmethod
     def create_dict(file):
-        d = {}
+        d = defaultdict(set)
         with open(file) as infile:
             for line in infile:
                 arr = line.split('->')
                 left = arr[0].strip()
                 right = arr[1].strip()
-                d[right] = left  # Bottom-up parsing
+                d[right].add(left)  # Bottom-up parsing
         return d
 
-    def parse(self, sentence):
+    def parse(self, sentence):  # TODO: create CFGs from the chart
         words = sentence.split(' ')
         chart = self.create_chart(len(words))
         self.fill_chart_with_dictionary(chart, words)
-        print(chart)
+        self.complete_chart(chart, words)
+        self.print_chart(chart)
 
     def create_chart(self, length):
         chart = []
         for i in range(length):
-            column = [None] * (i+1)
+            column = [None] * (i + 1)
             chart.append(column)
         return chart
 
     def fill_chart_with_dictionary(self, chart, words):
         for i in range(len(chart)):
-            chart[i][i] = self.dictionary[words[i]]
+            chart[i][i] = {CYKParser.ChartNode(x) for x in self.dictionary[words[i]]}
+
+    def complete_chart(self, chart, words):
+        for offset in range(1, len(words), 1):
+            for i in range(len(words) - offset):
+                self.complete_chart_field(chart, i + offset, i)
+
+    def complete_chart_field(self, chart, x, y):
+        left = x - 1
+        down = len(chart[x]) - 1
+        count = len(chart[x]) - (y + 1)
+        chart_field = set()
+        for i in range(count):
+            left_set = chart[left][y]
+            down_set = chart[x][down]
+            combinations = self.set_combinations(left_set, down_set)
+            result = set()
+            for c1, c2 in combinations:
+                parsed = self.grammar['{} {}'.format(c1.data, c2.data)]
+                if len(parsed) != 0:
+                    data = list(parsed)[0]
+                    same_data = [x for x in result if x.data == data]
+                    if len(same_data) > 0:
+                        same_data[0].append_pointers(c1, c2)
+                    else:
+                        result.add(CYKParser.ChartNode(data, c1, c2))
+            for c in result:
+                same_data = [x for x in chart_field if x.data == c.data]
+                if len(same_data) > 0:
+                    same_data[0].extend_pointers(c.pointers)
+                else:
+                    chart_field.add(c)
+            left -= 1
+            down -= 1
+        chart[x][y] = chart_field
+
+    def set_combinations(self, set1, set2):
+        result = set()
+        for x in set1:
+            for y in set2:
+                result.add((x, y))
+        return result
+
+    class ChartNode:
+        def __init__(self, data, left=None, right=None):
+            self.data = data
+            self.pointers = []
+            if left is not None and right is not None:
+                self.pointers.append((left, right))
+
+        def append_pointers(self, left, right):
+            self.pointers.append((left, right))
+
+        def extend_pointers(self, new_pointers):
+            self.pointers.extend(new_pointers)
+
+        def __str__(self):
+            return str(self.data)
+
+    def print_chart(self, chart):
+        line_start = 0
+        for row in range(len(chart)):
+            s = ' ' * line_start
+            for column in range(row, len(chart), 1):
+                string_set = {str(c) for c in chart[column][row]}
+                tmp = '{}' if len(string_set) == 0 else str(string_set)
+                s += tmp + ' ' * (15 - len(tmp))
+                if column == row:
+                    line_start = len(s)
+            print(s)
 
 
 if __name__ == '__main__':
